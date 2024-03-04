64fae5cd94007e5896d1083709d45c2e49139281
Jonathan <jonathancarranza70@gmail.com>
2024-03-03 21:09:51 -0300

    comiteando domingo a la noche

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Lista de tareas
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
